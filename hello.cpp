f51948449c8bd514e01f7ff7317bfe1fe5ad822b
Muhammad Subhan <soft.tec7490@gmail.com>
2026-01-19 21:16:40 +0500

    Add function to add new node mean to make a box in which the value can be added

--- hello.cpp
+++ hello.cpp
@@ -1,5 +1,19 @@
 #include<iostream>
 using namespace std;
+struct node
+{
+    int val;
+    node *next;
+};
+*node createNode(node *current, int value)
+{
+    node *n;
+    n=new node;
+    n->val=vlaue;
+    n->next=NULL;
+    current->next=n;
+    return n;
+}
 int main()
 {
     
