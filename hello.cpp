e0255b04f9078e25bc5ee0fe95b5d73cf7f8a6de
Muhammad Subhan <soft.tec7490@gmail.com>
2026-01-20 16:41:29 +0500

    Adding the function that output all the list

--- hello.cpp
+++ hello.cpp
@@ -5,16 +5,35 @@ struct node
     int val;
     node *next;
 };
-*node createNode(node *current, int value)
+node* createNode(node *current, int value)
 {
     node *n;
     n=new node;
-    n->val=vlaue;
+    n->val=value;
     n->next=NULL;
     current->next=n;
     return n;
 }
+void outputList(node *head)
+{
+    node *current;
+    current=head;
+     while(current != NULL)
+    {
+        cout<<current->val<<endl;
+        current=current->next;
+    }
+}
 int main()
 {
-    
+    node *current;
+    node *head;
+    head = new node;
+    current = head;
+    current->val = 4;
+    current = createNode(current,5);
+    current = createNode(current,6);
+    current = createNode(current,7);
+    outputList(head);
+   
 }
